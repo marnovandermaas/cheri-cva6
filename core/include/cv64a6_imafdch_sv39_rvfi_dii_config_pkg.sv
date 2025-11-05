086633dbb9d64f081c74d330d07c3661d0925788
Jonathan Woodruff <jonathan.woodruff@capabilitieslimited.co.uk>
2025-11-05 13:34:10 +0000

    Also move to 64-byte ICache line size.

--- core/include/cv64a6_imafdch_sv39_rvfi_dii_config_pkg.sv
+++ core/include/cv64a6_imafdch_sv39_rvfi_dii_config_pkg.sv
@@ -43,7 +43,7 @@ package cva6_config_pkg;
 
   localparam CVA6ConfigIcacheByteSize = 16384;
   localparam CVA6ConfigIcacheSetAssoc = 4;
-  localparam CVA6ConfigIcacheLineWidth = 128;
+  localparam CVA6ConfigIcacheLineWidth = 512;
   localparam CVA6ConfigDcacheByteSize = 32768;
   localparam CVA6ConfigDcacheSetAssoc = 8;
   localparam CVA6ConfigDcacheLineWidth = 512;
